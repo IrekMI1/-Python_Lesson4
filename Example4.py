ac989d4378ae60f51343ee43628f2ffd900a5398
Irek <general157@mail.ru>
2022-10-20 22:43:02 +0300

    example 4 added

---
+++ Example4.py
@@ -0,0 +1,43 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+""" from random import *
+pow = int(input('Введите натуральную степень: '))
+equation = ''
+for i in range(pow, -1, -1):
+    coeff = randint(0, 10)
+    if i not in [0, 1]:
+        equation += f'{coeff}*x^{i} + '
+    elif i == 1:
+        equation += f'{coeff}*x + '
+    else:
+        equation += f'{coeff} = 0'
+print(equation) """
+
+from random import *
+pow = int(input('Введите натуральную степень: '))
+equation = ''
+for i in range(pow, -1, -1):
+    coeff = randint(0, 100)
+    
+    if i not in [0, 1]:
+        if coeff not in [0, 1]:
+            equation += f'{coeff}*x^{i} + '
+        elif coeff == 1:
+            equation += f'x^{i} + '
+        else: continue
+
+    elif i == 1:
+        if coeff not in [0, 1]:
+            equation += f'{coeff}*x + '
+        elif coeff == 1:
+            equation += 'x + '
+        else: continue
+
+    else:
+        if coeff != 0:
+            equation += f'{coeff} = 0'
+        else:
+            equation = equation[0:len(equation) - 2] + '= 0'
+
+with open('file.txt', 'w') as data:
+    data.write(equation)
